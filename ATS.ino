4d6c70dce4ff7bfdf22f520b8c6d0349bfb73f06
Blackfire_TX <william.petefish@gmail.com>
2024-03-30 00:14:25 -0500

    Beerware license

--- ATS.ino
+++ ATS.ino
@@ -1,7 +1,12 @@
 /* Universal Genset Autostart Controller MK1
- * 
- * 
- * 
+  ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <william.petefish@gmail.com> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   -William Petefish
+ 
+ * Original THE BEER-WARE LICENSE by Poul-Henning Kamp
+  ----------------------------------------------------------------------------
  */
 
 // Initialize pins
